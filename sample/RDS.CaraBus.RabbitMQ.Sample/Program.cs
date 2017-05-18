0ff32645bf20ed9c1194862673255b7efd0adcee
Антон Красильщиков <a.krasilshchikov@rambler-co.ru>
2017-05-18 20:11:27 +0300

    basic samples implemented

--- sample/RDS.CaraBus.RabbitMQ.Sample/Program.cs
+++ sample/RDS.CaraBus.RabbitMQ.Sample/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Threading;
 
 namespace RDS.CaraBus.RabbitMQ.Sample
 {
@@ -20,6 +22,7 @@ namespace RDS.CaraBus.RabbitMQ.Sample
                 Console.WriteLine("1 - Single publisher and single subscriber");
                 Console.WriteLine("2 - Single publisher and multiple subscribers");
                 Console.WriteLine("3 - Single publisher and multiple subscribers that shares one message queue");
+                Console.WriteLine("4 - Scopes");
                 Console.WriteLine("empty string - exit");
 
                 Console.Write(">");
@@ -39,6 +42,9 @@ namespace RDS.CaraBus.RabbitMQ.Sample
                         break;
                     case "3":
                         SinglePublisherMultipleSubscribersThatSharesQueue();
+                        break;
+                    case "4":
+                        Scopes();
                         break;
                 }
             }
@@ -144,6 +150,79 @@ namespace RDS.CaraBus.RabbitMQ.Sample
 
                 caraBus.Stop();
             }
+        }
+
+        private static void Scopes()
+        {
+            Console.Clear();
+            Console.WriteLine("Scopes");
+            Console.WriteLine("There are two scopes declared for this sample: 'one' and 'two'");
+            Console.WriteLine("Enter '[scope]<space>message text' to send or empty string to exit:");
+
+            using (var caraBus = new CaraBus())
+            {
+                caraBus.Subscribe<Message>(m =>
+                {
+                    Console.WriteLine($"[scope 'one'] subscriber 1: received message: {m.Text}");
+                }, new SubscribeOptions { Scope = "one", Exclusive = true });
+
+                caraBus.Subscribe<Message>(m =>
+                {
+                    Console.WriteLine($"[scope 'one'] subscriber 2: received message: {m.Text}");
+                }, new SubscribeOptions { Scope = "one", Exclusive = true });
+
+                caraBus.Subscribe<Message>(m =>
+                {
+                    Console.WriteLine($"[scope 'two'] subscriber 1: received message: {m.Text}");
+                }, new SubscribeOptions { Scope = "two" });
+
+                caraBus.Subscribe<Message>(m =>
+                {
+                    Console.WriteLine($"[scope 'two'] subscriber 2: received message: {m.Text}");
+                }, new SubscribeOptions { Scope = "two" });
+
+                caraBus.Start();
+
+
+                var scopeOneMessage = "Hello for scope 'one'";
+                WriteDemoText($"one {scopeOneMessage}");
+                caraBus.PublishAsync(new Message { Text = scopeOneMessage }, new PublishOptions {Scope = "one" }).GetAwaiter().GetResult();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                var scopeTwoMessage = "Hello for scope 'two'";
+                WriteDemoText($"two {scopeTwoMessage}");
+                caraBus.PublishAsync(new Message { Text = scopeTwoMessage }, new PublishOptions { Scope = "two" }).GetAwaiter().GetResult();
+
+                while (true)
+                {
+                    var text = Console.ReadLine();
+                    if (text == string.Empty)
+                    {
+                        break;
+                    }
+
+                    var parts = text.Split(' ');
+
+                    var scope = parts[0];
+                    text = parts.Length > 1 
+                        ? parts.Skip(1).Aggregate((c, n) => c + " " + n)
+                        : String.Empty;
+
+                    caraBus.PublishAsync(new Message { Text = text }, new PublishOptions { Scope = scope }).GetAwaiter().GetResult();
+                }
+
+                caraBus.Stop();
+            }
+        }
+
+        private static void WriteDemoText(string text)
+        {
+            foreach (var c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+            Console.WriteLine("<enter>");
         }
 
     }
